bbb3b74bc8d2264ac9a0bc1337a38b055406c220
Novpa <agungnovpa@gmail.com>
2024-08-01 22:57:27 +0800

    add Clang-Format file

--- main.c
+++ main.c
@@ -12,118 +12,100 @@ void printSelectionSort(int b[], int lengthb);
 int insertionSort(int c[], int lengthc);
 void printInsertionSort(int c[], int lengthc);
 
-int main()
-{
-  // Bubble sort
-  int a[] = {3, 4, 5, 2, 6, 5, 8, 34, 56, 2, 11, 4, 5};
-  int lengtha = sizeof(a) / sizeof(a[0]);
-  // printf("%d\n ", length);
+int main() {
+    // Bubble sort
+    int a[] = {3, 4, 5, 2, 6, 5, 8, 34, 56, 2, 11, 4, 5};
+    int lengtha = sizeof(a) / sizeof(a[0]);
+    // printf("%d\n ", length);
 
-  bubbleSort(a, lengtha);
-  printBubbleSort(a, lengtha);
+    bubbleSort(a, lengtha);
+    printBubbleSort(a, lengtha);
 
-  int b[] = {3, 4, 5, 2, 6, 5, 8, 34, 56, 2, 11, 4, 5};
-  int lengthb = sizeof(b) / sizeof(b[0]);
+    int b[] = {3, 4, 5, 2, 6, 5, 8, 34, 56, 2, 11, 4, 5};
+    int lengthb = sizeof(b) / sizeof(b[0]);
 
-  selectionSort(b, lengthb);
-  printSelectionSort(b, lengthb);
+    selectionSort(b, lengthb);
+    printSelectionSort(b, lengthb);
 
-  // Selection Sort
-  int c[] = {3, 4, 5, 2, 6, 5, 8, 34, 56, 2, 11, 4, 5};
-  int lengthc = sizeof(c) / sizeof(c[0]);
+    // Selection Sort
+    int c[] = {3, 4, 5, 2, 6, 5, 8, 34, 56, 2, 11, 4, 5};
+    int lengthc = sizeof(c) / sizeof(c[0]);
 
-  insertionSort(c, lengthc);
-  printInsertionSort(c, lengthc);
+    insertionSort(c, lengthc);
+    printInsertionSort(c, lengthc);
 
-  return 0;
+    return 0;
 }
 
 // NOTE Bouble sort method DONE
 
-int bubbleSort(int a[], int lengtha)
-{
+int bubbleSort(int a[], int lengtha) {
 
-  for (int i = 0; i < lengtha - 1; i++)
-  {
-    for (int j = 0; j < lengtha - 1; j++)
-    {
-      if (a[j] > a[j + 1])
-      {
-        // Swap
-        int temp = a[j];
-        a[j] = a[j + 1];
-        a[j + 1] = temp;
-      }
+    for (int i = 0; i < lengtha - 1; i++) {
+        for (int j = 0; j < lengtha - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                // Swap
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
     }
-  }
 }
 
-void printBubbleSort(int a[], int lengtha)
-{
-  printf("Bubble sort method: ");
-  for (int i = 0; i < lengtha; i++)
-  {
-    printf("%d, ", a[i]);
-  }
+void printBubbleSort(int a[], int lengtha) {
+    printf("Bubble sort method: ");
+    for (int i = 0; i < lengtha; i++) {
+        printf("%d, ", a[i]);
+    }
 }
 
 // NOTE Selection sort mothod: DONE
 
-int selectionSort(int b[], int lengthb)
-{
-  for (int i = 0; i < lengthb - 1; i++)
-  {
-    int minimum = i;
-    for (int j = i + 1; j < lengthb; j++)
-    {
-      if (b[j] < b[minimum])
+int selectionSort(int b[], int lengthb) {
+    for (int i = 0; i < lengthb - 1; i++) {
+        int minimum = i;
+        for (int j = i + 1; j < lengthb; j++) {
+            if (b[j] < b[minimum])
 
-        minimum = j;
+                minimum = j;
+        }
+        if (minimum != i) {
+            int temp = b[i];
+            b[i] = b[minimum];
+            b[minimum] = temp;
+        }
     }
-    if (minimum != i)
-    {
-      int temp = b[i];
-      b[i] = b[minimum];
-      b[minimum] = temp;
-    }
-  }
 }
 
-void printSelectionSort(int b[], int lengthb)
-{
-  printf("\nSelection Sort Method: ");
+void printSelectionSort(int b[], int lengthb) {
+    printf("\nSelection Sort Method: ");
 
-  for (int i = 0; i < lengthb; i++)
-  {
-    printf("%d, ", b[i]);
-  }
+    for (int i = 0; i < lengthb; i++) {
+        printf("%d, ", b[i]);
+    }
 }
 
 // NOTE Insertion Sort Method
 
-int insertionSort(int c[], int lengthc)
-{
+int insertionSort(int c[], int lengthc) {
 
-  int key;
-  int j;
-  for (int i = 1; i < lengthc; i++)
-  {
-    key = c[i];
-    j = i - 1;
-    while (j >= 0 && c[j] > key)
-    {
-      c[j + 1] = c[j];
-      j = j - 1;
+    int key;
+    int j;
+    for (int i = 1; i < lengthc; i++) {
+        key = c[i];
+        j = i - 1;
+        while (j >= 0 && c[j] > key) {
+            c[j + 1] = c[j];
+            j = j - 1;
+        }
+        c[j + 1] = key;
     }
-    c[j + 1] = key;
-  }
 }
 
-void printInsertionSort(int c[], int lengthc)
-{
-  printf("\nInsertion sort method: ");
-  for (int i = 0; i < lengthc; i++)
-  {
-    printf("%d ", c[i]);
-  }
+void printInsertionSort(int c[], int lengthc) {
+    printf("\nInsertion sort method: ");
+    for (int i = 0; i < lengthc; i++) {
+        printf("%d ", c[i]);
+    }
 }
--- merge.c
+++ merge.c
@@ -1,8 +1,71 @@
 #include <stdio.h>
 
-int main()
-{
+void merge(int arr[], int left, int mid, int right) {
+    int mergeLeft = mid - left + 1;
+    int mergeRight = right - mid;
 
-  printf("Testing");
-  return 0;
+    // Devide it in to 2 arrays (TEMPORARY ARRAYS)
+    int arrLeft[mergeLeft], arrRight[mergeRight];
+
+    // Copying the the data in to the LEFT ARRAY
+    for (int i = 0; i < mergeLeft; i++) {
+        arrLeft[i] = arr[left + i];
+    }
+
+    // Copying the data in to the RIGHT ARRAY
+    for (int j = 0; j < mergeRight; j++) {
+        arrRight[j] = arr[mid + 1 + j];
+    }
+
+    // Merge the temporary arrays back in to ORIGINAL ARRAY
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < mergeLeft && j < mergeRight) {
+        if (arrLeft[i] < arrRight[j]) {
+            arr[k] = arrLeft[i];
+            i++;
+        } else {
+            arr[k] = arrRight[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < mergeLeft) {
+        arr[k] = arrLeft[i];
+        i++;
+        k++;
+    }
+
+    while (j < mergeRight) {
+        arr[k] = arrRight[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+
+        int middle = left + (right - left) / 2;
+
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+        merge(arr, left, middle, right);
+    }
+}
+
+int main() {
+    // NOTE: BASE CODE
+    int arr[] = {4, 3, 7, 4, 8, 6, 43, 42, 77, 56};
+    int length = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, length - 1);
+    printf("Merged sort method: ");
+    for (int i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
 }
