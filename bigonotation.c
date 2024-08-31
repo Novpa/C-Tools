c03085b104700de06e450ef4a4ff117145179fbf
Novpa <agungnovpa@gmail.com>
2024-08-31 19:20:22 +0800

    Linear search

--- bigonotation.c
+++ bigonotation.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
-int main() {
+int linearSearcher(int arr[], int left, int right, int key) {
+    if (left >= right)
+        return -1;
 
+    if (key == arr[left])
+        return left;
+
+    return linearSearcher(arr, left + 1, right, key);
+}
+
+int main() {
+    int key;
+
+    int arr[] = {10, 20, 30, 40, 50, 23, 45, 66};
+    int length = sizeof(arr) / sizeof(arr[0]);
+
+    int left = 0;
+
+    printf("Enter the number: ");
+    scanf("%d", &key);
+
+    int result = linearSearcher(arr, left, length, key);
+
+    printf("%d", result);
     return 0;
 }
