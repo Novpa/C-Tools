e44398c5391a8a40c7db12c8d37cc5d85611cad1
Novpa <agungnovpa@gmail.com>
2024-09-13 13:39:04 +0800

    finalize merge-sort

--- algo-design/devide-conquer/merge-sort/merge-sort.c
+++ algo-design/devide-conquer/merge-sort/merge-sort.c
@@ -22,6 +22,38 @@ void merge(int arr[], int left, int mid, int right) {
     for (int j = 0; j < sizeRight; j++) {
         arrRight[j] = arr[mid + 1 + j];
     }
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < sizeLeft && j < sizeRight) {
+        if (arrLeft[i] <= arrRight[j]) {
+            arr[k] = arrLeft[i];
+            i++;
+        } else {
+            arr[k] = arrRight[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copying remaining element to the array and will be compare later on (left
+    // side)
+    while (i < sizeLeft) {
+        arr[k] = arrLeft[i];
+        i++;
+        k++;
+    }
+
+    // Copying remaining element to the array and will be compare later on
+    // (right    side)
+
+    while (j < sizeRight) {
+        arr[k] = arrRight[j];
+        j++;
+        k++;
+    }
 }
 
 // Merge & Sort
@@ -29,24 +61,37 @@ int mergeSort(int array[], int left, int right) {
 
     // Base case
     // This works once the element has one or no element, so It's already
-    // sorted.
-    if (left >= right)
-        return;
+    // sorted or the left is greater than the right.
+    if (left < right) {
 
-    // Calculate the middle index of the current subarray
-    int mid = left + (right - left) / 2;
+        // Calculate the middle index of the current subarray
+        int mid = left + (right - left) / 2;
 
-    // Call the function recursively until touch the `base case`
-    mergeSort(arr, left, mid);
-    mergeSort(arr, mid + 1, right);
+        // Call the function recursively until touch the `base case`
+        mergeSort(array, left, mid);
+        mergeSort(array, mid + 1, right);
 
-    // Merge all the sorted value
-    merge(arr, left, mid, right);
+        // Merge all the sorted value
+        merge(array, left, mid, right);
+    }
+}
+
+void printArr(int arr[], int length) {
+    for (int i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    }
 }
 
 int main() {
 
     int arr[] = {3, 12, 2, 43, 5, 17};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    int right = length - 1;
+
+    printArr(arr, length);
+    printf("\n");
+    mergeSort(arr, 0, right);
+    printArr(arr, length);
 
     return 0;
 }
