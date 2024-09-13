5f66b18e96873a589ae68dcdc052ca0942b05d73
Novpa <agungnovpa@gmail.com>
2024-09-13 12:35:31 +0800

    merge sort

--- algo-design/devide-conquer/merge-sort/merge-sort.c
+++ algo-design/devide-conquer/merge-sort/merge-sort.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+// Merge function
+void merge(int arr[], int left, int mid, int right) {
+
+    // Calculate the size of the left and the right array
+    int sizeLeft = mid - left + 1;
+    int sizeRight = right - mid;
+
+    // Declare a empty array with the size above to store the temporarly array
+    // (both in left and right sides)
+    int arrLeft[sizeLeft], arrRight[sizeRight];
+
+    // Copy all the data to the temp array
+    // LEFT
+    for (int i = 0; i < sizeLeft; i++) {
+        arrLeft[i] = arr[left + i];
+    }
+
+    // This will copy the value right after the middle value of the array and
+    // then increment it by j
+    for (int j = 0; j < sizeRight; j++) {
+        arrRight[j] = arr[mid + 1 + j];
+    }
+}
+
 // Merge & Sort
 int mergeSort(int array[], int left, int right) {
 
